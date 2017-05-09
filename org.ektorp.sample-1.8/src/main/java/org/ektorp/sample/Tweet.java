588dd139628dd66510319df25a476adcb55d4617
xxiang2 <xxiang2@student.unimelb.edu.au>
2017-05-09 14:19:10 +1000

    Update Tweet.java

--- org.ektorp.sample-1.8/src/main/java/org/ektorp/sample/Tweet.java
+++ org.ektorp.sample-1.8/src/main/java/org/ektorp/sample/Tweet.java
@@ -26,15 +26,34 @@ public class Tweet {
 			//CouchDbConnector tweet_test = null;
 			//tweet_test = dbInstance.createConnector("tweet_test", true);
 			System.out.println("****Test Tweet****");
-			tweet_test=ConnectorDb.createCouchDbConnectorTweet();
-			TweetRepository tweet_re_test= new TweetRepository(tweet_test);
-			int n=tweet_re_test.geMelCycle();
-			System.out.println(n);
-			List<JsonNode> doc_all = new ArrayList<JsonNode>();
-			doc_all=tweet_re_test.getMelCycle1();
-			for (JsonNode doc_s : doc_all) {
-				System.out.println(doc_s.get("text").toString().replaceAll("\"", ""));
-			}
+		
+		tweet_test=ConnectorDb.createCouchDbConnectorTweet();
+		TweetRepository tweet_re_test= new TweetRepository(tweet_test);
+		
+		int n=tweet_re_test.geMelCycle();
+		System.out.println(n);
+		System.out.println("****Test 1 ends****");
+		
+		List<JsonNode> doc_all = new ArrayList<JsonNode>();
+		doc_all=tweet_re_test.getMelCycleCity();
+		for (JsonNode doc_s : doc_all) {
+			System.out.println(doc_s.get("text").toString().replaceAll("\"", ""));
+		}
+		System.out.println("****Test 2 ends****");
+		
+		List<JsonNode> doc_weekday = new ArrayList<JsonNode>();
+		doc_weekday=tweet_re_test.getMelWeekday();
+		for (JsonNode doc_s : doc_weekday) {
+			System.out.println(doc_s.get("text").toString().replaceAll("\"", "")+" "+doc_s.get("created_at"));
+		}
+		System.out.println("****Test 3 ends****");
+		
+		List<JsonNode> doc_weekend = new ArrayList<JsonNode>();
+		doc_weekend=tweet_re_test.getMelWeekend();
+		for (JsonNode doc_s : doc_weekend) {
+			System.out.println(doc_s.get("text").toString().replaceAll("\"", "")+" "+doc_s.get("created_at"));
+		}
+		System.out.println("****Test 4 ends****");
 /*			doc_ids = tweet_test.getAllDocIds();*/
 			/*for (String doc_id : doc_ids) {
 				JsonNode doc_s = tweet_test.get(JsonNode.class, doc_id);
